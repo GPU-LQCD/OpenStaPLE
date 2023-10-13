042ea70e6ac9f9ab29fd056b4db93ba2f238a178
kzambello <kevin.zambello@pi.infn.it>
2023-10-13 09:31:29 +0200

    Add sanity check for VerbosityLv, use 20.9 compiler on Leonardo.

--- build/script_compile_Open_StaPLE_leo.sh
+++ build/script_compile_Open_StaPLE_leo.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+#module load nvhpc/23.1
+#module load openmpi/4.1.4--nvhpc--23.1-cuda-11.8
+
+module load /leonardo/home/userexternal/ecalore0/nvidia/hpc_sdk/modulefiles/nvhpc/20.9
+
+OPENMPI_LIB=/leonardo/home/userexternal/ecalore0/nvidia/hpc_sdk/Linux_x86_64/20.9/comm_libs/openmpi/openmpi-3.1.5
+
+export MPILIB=$OPENMPI_LIB/lib
+export MPIINC=$OPENMPI_LIB/include
+
 # LOCAL LIB PATH
 my_lib_path=$( echo "${LIBRARY_PATH}" | cut -d ':' -f 1 ) 
 
-# CONFIGURE PARAMETERS SET FOR OPTIMIZED COMPILATION ON LEONARDO
+# CONFIGURE PARAMETERS SET FOR OPTIMIZED COMPILATION ON M100 (GPU = Nvidia Volta (V100) -> --ta=tesla,cc70) 
 C_comp=pgcc 
-C_comp_flags="-acc=noautopar,gpu -v -O0 -Minfo=all -gpu=cc80 -DUSE_MPI_CUDA_AWARE -I${OPENMPI_INCLUDE} -U__GNUC__"
-linker_flags="-acc=noautopar,gpu -v -O0 -Minfo=all -gpu=cc80 -lmpi -L${OPENMPI_LIB} -L${my_lib_path} -U__GNUC__" 
+C_comp_flags="-acc=noautopar -v -O3 -Minfo=all -ta=tesla:cc80 -DUSE_MPI_CUDA_AWARE -I${MPIINC} -U__GNUC__" 
+linker_flags="-acc=noautopar -v -O3 -Minfo=all -ta=tesla:cc80 -lmpi -L${MPILIB}" #-L${my_lib_path} -U__GNUC__" 
 CXX_comp=pgc++ 
 CXX_comp_flags="-O3 -U__GNUC__"
 cur_dir="--prefix=${PWD}" 
 
 # LOAD PYTHON2.6
-module unload python
+#module unload python
+#module load profile/global
+#module load pgi
 
 # CONFIGURE + COMPILE
 # if autotools are not built, build them
